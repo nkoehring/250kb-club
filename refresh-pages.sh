3f8e4d349d8bfd92d812912a6bd0129a2d8cb2b0
koehr <n@koehr.in>
2020-11-23 16:10:13 +0100

    updates URLs

--- refresh-pages.sh
+++ refresh-pages.sh
@@ -4,4 +4,4 @@ node ./compile-list.js &&
 yarn build &&
 scp -r public/* 250kb.club:/srv/http/250kb.club/ &&
 git commit -am 'updates URLs' &&
-git push
+git push sourcehut main && git push origin main
