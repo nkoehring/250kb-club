b0b75972d7cf5fd8b94948b4afddb3502c86fe03
koehr <n@koehr.in>
2021-01-14 14:38:05 +0100

    whoops, typo

--- src/components/MainFooter.svelte
+++ src/components/MainFooter.svelte
@@ -5,7 +5,7 @@
 <footer id="main-footer">
   <p>
     Made with &hearts; for a performant web by <Link href="https://koehr.in">Norman Köhring</Link>.
-    Inspired by <Link href="https://uglyduck.ca">Bradley Taunt</Link>s <Link href="https://1MB.club">1MB.club</Link>
+    Inspired by <Link href="https://uglyduck.ca">Bradley Taunt</Link>'s <Link href="https://1MB.club">1MB.club</Link>
     <br/>
     The code of this page is open source. You can find it on <Link href="https://github.com/nkoehring/250kb-club">Github</Link>
     and <Link href="https://sr.ht/~koehr/the-250kb-club/">Sourcehut</Link>.
